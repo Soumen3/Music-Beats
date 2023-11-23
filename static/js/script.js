a1da693fa16db8b01d19413e05897b54918a1ba7
Soumen3 <soumensamanta112233@gmail.com>
2023-11-23 17:17:25 +0530

    history

---
+++ static/js/script.js
@@ -0,0 +1,11 @@
+
+// var message_box=document.getElementById('message-box');
+
+function hide(e){
+    document.getElementById('message-box').style.display='none'
+}
+
+
+setTimeout(function() {
+    document.getElementById('message-box').style.display = 'none';
+}, 5000);
