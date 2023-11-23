2c33a06a12f3832370462a49d1579056c253af83
Soumen3 <soumensamanta112233@gmail.com>
2023-11-23 15:03:21 +0530

    add to favourite complete

---
+++ static/js/listne_song.js
@@ -0,0 +1,18 @@
+// console.log('console');
+
+// $(document).on('submit', '#fav-form', function(event) {
+//     event.preventDefault();
+//     var form_data = $(this).serialize();
+//     $.ajax({
+//         url: url,
+//         method: 'POST',
+//         data: form_data,
+//         success: function(response) {
+//             // Handle success response
+//         },
+//         error: function(response) {
+//             // Handle error response
+//         }
+//     });
+// });
+
